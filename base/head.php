a8341394f95b7ac94c0f4c00c6e28dcb6ec9075b
Wilco van Meppelen <wilco@w3worx.nl>
2018-10-26 18:27:33 +0200

    no message

--- base/head.php
+++ base/head.php
@@ -11,7 +11,7 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://localhost/team1/style/public/stylesheets/app.css">
+    <link rel="stylesheet" href="style/public/stylesheets/app.css">
 
   </head>
   <body>
