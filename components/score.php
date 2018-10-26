a3c70fc4f805cf726f812a3da0d09efec77640ac
Wilco van Meppelen <wilco@w3worx.nl>
2018-10-26 20:58:35 +0200

    score pagina

--- components/score.php
+++ components/score.php
@@ -1,8 +1,25 @@
 <section id="score">
-  <div class="image">
-    <img src="style/public/images/confetti.png" alt="">
-  </div>
+  <?php
+    if($_GET['score'] < 6){
+      echo'
+      <div class="image">
+        <img src="style/public/images/hank_angry.png" alt="">
+      </div>
+      ';
+      echo'
+      <div class="score">Your mark: 7 '.$_GET["score"].' </div>
+      ';
+    } else {
+      echo'
+      <div class="image">
+        <img src="style/public/images/confetti.png" alt="">
+      </div>
+      ';
+      echo'
+      <div class="score">Your mark: 7 '.$_GET["score"].' </div>
+      ';
+    }
+  ?>
 
-  <div class="score">Your mark: 7<?php echo $_GET['score']; ?></div>
   <h1>Today's winner:<br><span>Gerwald</span></h1>
 </section>
