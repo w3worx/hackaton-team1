9cc641eaabce37920c34188ce19bae4e050c960e
Niels Buijtenhuis <niels@w3worx.nl>
2018-10-26 18:27:00 +0200

    screens

---
+++ components/login.php
@@ -0,0 +1,13 @@
+<div id="login">
+
+  <div class="form">
+    <h1>Login</h1>
+    <form action="/">
+      <input type="text" name="hank-username" placeholder="Username" autocomplete="off" autocomplete="false">
+      <input type="password" name="hank-password" placeholder="Password" autocomplete="off">
+      <input type="submit" value="login">
+    </form>
+
+  </div>
+
+</div>
