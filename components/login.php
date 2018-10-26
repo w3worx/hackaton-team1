043e1795cd942d8ae5559c3b00216586ca47b36a
Wilco van Meppelen <wilco@w3worx.nl>
2018-10-26 19:05:23 +0200

    login styling

--- components/login.php
+++ components/login.php
@@ -1,11 +1,12 @@
 <div id="login">
 
   <div class="form">
+    <img src="style/public/images/logo.svg" alt="">
     <h1>Login</h1>
     <form action="/">
       <input type="text" name="hank-username" placeholder="Username" autocomplete="off" autocomplete="false">
       <input type="password" name="hank-password" placeholder="Password" autocomplete="off">
-      <input type="submit" value="login">
+      <input type="submit" value="login" class="button green large">
     </form>
 
   </div>
